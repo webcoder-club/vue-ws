7aed66c9515a41007b07f3a3d48ff535f3a50ba6
Igor Somov <isomov@avito.ru>
2017-05-22 13:26:39 +0300

    Move to separate components

---
+++ frontend/src/components/Bottom.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>VueJS Workshop</strong>.
+                </p>
+                <p>
+                    <a class="icon" href="https://github.com/webcoder-club/vue-ws-beginner">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="field has-addons">
+        <p class="control">
+            <input v-model="query" class="input" type="text" placeholder="Filter">
+        </p>
+        <p class="control">
+            <a class="button is-primary" v-on:click="search">
+                Search
+            </a>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'search',
+        data() {
+            return {
+                query: ''
+            }
+        },
+        methods: {
+            search() {
+                Event.$emit('search', this.query);
+            }
+        }
+    }
+</script>
